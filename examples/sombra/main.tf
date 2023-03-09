5ebaeee6f21edc7beadc9b9e6588f40b052dd552
Linh Chau <94718676+linh-transcend@users.noreply.github.com>
2023-03-09 19:36:07 +0000

    add comment on where to get sombra url

--- examples/sombra/main.tf
+++ examples/sombra/main.tf
@@ -1,3 +1,4 @@
 data "transcend_sombra" "test" {
+  // sombra URL can be retrieved from the TRANSCEND INGRESS URL column in the admin dashboard under Infrastructure > Sombra
   url = "https://test.sombra.trancsend.com/"
 }
