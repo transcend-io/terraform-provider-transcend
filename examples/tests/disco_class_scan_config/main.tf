0e7434e9b4a64216d53c6a954fbfc5123b36a407
Alessandro Gagliardi <alessandro@transcend.io>
2025-09-15 20:54:37 +0000

    examples/tests

---
+++ examples/tests/disco_class_scan_config/main.tf
@@ -0,0 +1,70 @@
+terraform {
+  required_providers {
+    transcend = {
+      version = "0.19.0"
+      source  = "transcend.com/cli/transcend"
+    }
+  }
+}
+
+provider "transcend" {
+  url = "https://api.staging.transcen.dental/"
+}
+
+variable "title" {}
+variable "disco_class_scan_config" {
+  type = object({
+    enabled                    = bool
+    schedule_frequency_minutes = number
+    schedule_start_at          = string
+  })
+}
+
+resource "transcend_data_silo" "silo" {
+  type            = "amazonDynamodb"
+  title           = var.title
+  skip_connecting = true
+}
+
+resource "transcend_data_silo_connection" "connection" {
+  data_silo_id = transcend_data_silo.silo.id
+
+  plaintext_context {
+    name  = "role"
+    value = "TranscendAWSIntegrationRole"
+  }
+
+  plaintext_context {
+    name  = "accountId"
+    value = "590309927493"
+  }
+
+  plaintext_context {
+    name  = "region"
+    value = "eu-west-1"
+  }
+
+  // Enable item-level access
+  plaintext_context {
+    name  = "database"
+    value = "true"
+  }
+}
+
+resource "transcend_disco_class_scan_config" "disco_class_scan_config" {
+  data_silo_id = transcend_data_silo.silo.id
+
+  enabled                    = var.disco_class_scan_config["enabled"]
+  schedule_frequency_minutes = var.disco_class_scan_config["schedule_frequency_minutes"]
+  schedule_start_at          = var.disco_class_scan_config["schedule_start_at"]
+
+  depends_on = [transcend_data_silo_connection.connection]
+}
+
+output "awsExternalId" {
+  value = transcend_data_silo.silo.aws_external_id
+}
+
+output "dataSiloId" {
+  value = transcend_data_silo.silo.id
+}
