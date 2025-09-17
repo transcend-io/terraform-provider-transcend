99cd1dff051b127ce3c339ff4a64c8909f5015f9
Alessandro Gagliardi <alessandro@transcend.io>
2025-09-17 12:41:43 +0000

    plugins

--- transcend/resource_disco_class_scan_config_test.go
+++ transcend/resource_disco_class_scan_config_test.go
@@ -23,14 +23,15 @@ func prepareDiscoClassScanConfigOptions(t *testing.T, vars map[string]interface{
 	return terraformOptions
 }
 
-func deployDiscoClassScanConfig(t *testing.T, terraformOptions *terraform.Options) (types.DataSilo, types.DiscoClassScanConfig) {
+func deployDiscoClassScanConfig(t *testing.T, terraformOptions *terraform.Options) (types.DataSilo, types.DiscoClassScanConfig, []types.Plugin) {
 	// TODO: Use the Idempotent version eventually
 	terraform.InitAndApply(t, terraformOptions)
 	// terraform.InitAndApplyAndIdempotent(t, terraformOptions)
 	assert.NotEmpty(t, terraform.Output(t, terraformOptions, "dataSiloId"))
 	silo := lookupDataSilo(t, terraform.Output(t, terraformOptions, "dataSiloId"))
 	discoClassScanConfig := lookupDataSiloDiscoClassScanConfig(t, terraform.Output(t, terraformOptions, "dataSiloId"))
-	return silo, discoClassScanConfig
+	plugins := lookupDataSiloPlugin(t, terraform.Output(t, terraformOptions, "dataSiloId"))
+	return silo, discoClassScanConfig, plugins
 }
 
 func TestCanUseSeparateDiscoClassScanConfigResource(t *testing.T) {
@@ -44,11 +45,26 @@ func TestCanUseSeparateDiscoClassScanConfigResource(t *testing.T) {
 		},
 	})
 	defer terraform.Destroy(t, options)
-	silo, discoClassScanConfig := deployDiscoClassScanConfig(t, options)
+	silo, discoClassScanConfig, plugins := deployDiscoClassScanConfig(t, options)
 	assert.Equal(t, graphql.String(t.Name()), silo.Title)
 	assert.NotEmpty(t, terraform.Output(t, options, "awsExternalId"))
 	assert.Equal(t, types.DiscoClassScanType("FULL_SCAN"), discoClassScanConfig.Type)
 	assert.Equal(t, graphql.Boolean(true), discoClassScanConfig.Enabled)
 	assert.Equal(t, graphql.Int(120*1000*60), discoClassScanConfig.ScheduleFrequency) // API returns milliseconds
 	assert.Equal(t, graphql.String("2122-09-06T17:51:13.000Z"), discoClassScanConfig.ScheduleStartAt)
+	assert.Len(t, plugins, 2)
+	hasSchemaDiscovery := false
+	hasContentClassification := false
+	for _, plugin := range plugins {
+		assert.True(t, bool(plugin.Enabled))
+		if plugin.Type == "SCHEMA_DISCOVERY" {
+			hasSchemaDiscovery = true
+		}
+		if plugin.Type == "CONTENT_CLASSIFICATION" {
+			hasContentClassification = true
+		}
+		assert.NotEmpty(t, plugin.ID)
+	}
+	assert.True(t, hasSchemaDiscovery)
+	assert.True(t, hasContentClassification)
 }
